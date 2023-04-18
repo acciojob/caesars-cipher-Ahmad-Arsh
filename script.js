8d971222092c8b756e996808f224dda254b27367
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-18 16:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-// Your Script here.
+function rot13(str) {
+  let decodedStr = "";
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-	for (let i = 0; i < str.length; i++) {
-        let charCode = str.charCodeAt(i);
-        if (charCode >= 65 && charCode <= 90) {
-            result += String.fromCharCode((charCode - 65 + 13) % 26 + 65);
-        } else {
-            result += str[i];
-        }
+    if (charCode >= 65 && charCode <= 90) {
+      // Convert the character code to the corresponding decoded character code
+      charCode = ((charCode - 65 + 13) % 26) + 65;
     }
 
-  return ;//return decodedArr
+    decodedStr += String.fromCharCode(charCode);
+  }
+
+  return decodedStr;
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-module.exports = rot13;
